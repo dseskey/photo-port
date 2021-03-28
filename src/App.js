c928d7ed02a45377c5db73679b7c6720e515155f
seskey <Seskey@Einstein.local>
2021-03-28 17:20:59 -0400

    added photo list and manage changes via states passed down from app.js due to nav clicks

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ import Gallery from './components/Gallery';
 function App() {
   const [categories] = useState([
     {
-        name: "commecrial",
+        name: "commercial",
         description:
             "Photos of grocery stores, food trucks, and other commercial projects",
     },
     { name: "portraits", description: "Portraits of people in my life" },
-    { name: "Food", description: "Delicious delicacies" },
+    { name: "food", description: "Delicious delicacies" },
     {
         name: "landscape",
         description: "Fields, farmhouses, waterfalls, and the beauty of nature",
@@ -20,12 +20,11 @@ function App() {
 ]);
 
 const [currentCategory, setCurrentCategory] = useState(categories[0]);
-
   return (
     <div>
       <Nav categories={categories} setCurrentCategory={setCurrentCategory} currentCategory={currentCategory}/>
       <main>
-        <Gallery/>
+      <Gallery {...currentCategory}></Gallery>
         <About/>
       </main>
     </div>
