0a268335be96f7007867153e67eb71e0cbbfbdf3
seskey <Seskey@Einstein.local>
2021-04-02 19:57:41 -0400

    update

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -99,6 +99,7 @@ const PhotoList = ({ category }) => {
         },
     ]);
     const currentPhotos = photos.filter((photo) => photo.category === category);
+    
     return (
         <div>
             <div className="flex-row">
