78a147acda73244054b579e8a3254a730d1aaefb
seskey <Seskey@Einstein.local>
2021-04-02 20:44:11 -0400

    added conditional rendering for gallery and contact form

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Gallery from './components/Gallery';
 import ContactForm from './components/Contact';
 
 function App() {
+  const [contactSelected, setContactSelected] = useState(false);
   const [categories] = useState([
     {
       name: "commercial",
@@ -23,11 +24,22 @@ function App() {
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
   return (
     <div>
-      <Nav categories={categories} setCurrentCategory={setCurrentCategory} currentCategory={currentCategory} />
+      <Nav 
+        categories={categories} 
+        setCurrentCategory={setCurrentCategory} 
+        currentCategory={currentCategory}   
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected} 
+        />
       <main>
-        <ContactForm />
-        <Gallery {...currentCategory}></Gallery>
-        <About />
+        {!contactSelected ? 
+                <>
+                <Gallery {...currentCategory}></Gallery>        
+                <About />
+                </>
+                : <ContactForm />
+
+        }
       </main>
     </div>
   );
